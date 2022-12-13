10484f8f0f10bd4f0dfc89406afa34cbecf61498
mdcutone <matthew.cutone@gmail.com>
2022-12-13 15:12:53 -0500

    RF: Added file header

--- psychopy_connect/__init__.py
+++ psychopy_connect/__init__.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Originally from the PsychoPy library
+# Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
+# Distributed under the terms of the GNU General Public License (GPL).
+
+"""Interfaces for serial and parallel devices.
+"""
+
+__version__ = '0.0.1'
+
+# ------------------------------------------------------------------------------
+# Generic serial and parallel interfaces
+#
+
 from .serialdevice import SerialDevice
 from .parallel import setPortAddress, setPin, setData
+
+# ------------------------------------------------------------------------------
+# Components for Builder
+#
+
 from .components import SerialOutComponent, ParallelOutComponent
